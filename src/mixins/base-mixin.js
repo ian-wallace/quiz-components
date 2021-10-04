74c743bf7be1b4ecfa43bf367ebb9846b4577925
louiscoste <66270369+louiscoste@users.noreply.github.com>
2021-10-04 14:36:01 -0400

    [increment patch] Changing locale import to fix BSI break (#38)
    * Added import of all langterms
    
    * fixed tests
    
    * Fixed linting
    
    * Changing locale import to fix BSI break

--- src/mixins/base-mixin.js
+++ src/mixins/base-mixin.js
@@ -5,17 +5,64 @@ export const BaseMixin = superclass => class extends RtlMixin(LocalizeMixin(supe
 	static async getLocalizeResources(langs) {
 		let translations;
 		for await (const lang of langs) {
-			try {
-				let locale;
-				if (lang === 'en-US' || lang === 'en-us') {
-					locale = '../../locales/en.js';
-				} else {
-					locale = `../../locales/${lang}.js`;
-				}
-
-				translations = await import(locale);
-			} catch (_) {
-				console.error(`Locale ${lang} not found`);
+			switch (lang) {
+				case 'en':
+					translations = await import('../../locales/en.js');
+					break;
+				case 'en-US':
+					translations = await import('../../locales/en.js');
+					break;
+				case 'ar-SA':
+					translations = await import('../../locales/ar-SA.js');
+					break;
+				case 'cy-GB':
+					translations = await import('../../locales/cy-GB.js');
+					break;
+				case 'da-DK':
+					translations = await import('../../locales/da-DK.js');
+					break;
+				case 'de-DE':
+					translations = await import('../../locales/de-DE.js');
+					break;
+				case 'es-ES':
+					translations = await import('../../locales/es-ES.js');
+					break;
+				case 'es-MX':
+					translations = await import('../../locales/es-MX.js');
+					break;
+				case 'fr-CA':
+					translations = await import('../../locales/fr-CA.js');
+					break;
+				case 'fr-FR':
+					translations = await import('../../locales/fr-FR.js');
+					break;
+				case 'fr-ON':
+					translations = await import('../../locales/fr-ON.js');
+					break;
+				case 'ja-JP':
+					translations = await import('../../locales/ja-JP.js');
+					break;
+				case 'ko-KR':
+					translations = await import('../../locales/ko-KR.js');
+					break;
+				case 'nl-NL':
+					translations = await import('../../locales/nl-NL.js');
+					break;
+				case 'pt-BR':
+					translations = await import('../../locales/pt-BR.js');
+					break;
+				case 'sv-SE':
+					translations = await import('../../locales/sv-SE.js');
+					break;
+				case 'tr-TR':
+					translations = await import('../../locales/tr-TR.js');
+					break;
+				case 'zh-CN':
+					translations = await import('../../locales/zh-CN.js');
+					break;
+				case 'zh-TW':
+					translations = await import('../../locales/zh-TW.js');
+					break;
 			}
 
 			if (translations && translations.val) {
