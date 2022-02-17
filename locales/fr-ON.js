e564f3051c3ea93ca43508776917b903749567b4
JenkinsReader <resupport@desire2learn.com>
2022-02-17 13:45:54 -0500

    Translation Updates

--- locales/fr-ON.js
+++ locales/fr-ON.js
@@ -3,7 +3,7 @@ export const val = {
 	'buttonUpdate': 'Mettre à jour',
 	'externalActivity': 'Activité externe',
 	'inputLabelPoints': 'Points',
-	'mainBodyDescription': 'La mise à jour des détails de ces questions sera appliquée au questionnaire dans Brightspace, mais n\'aura pas d\'incidence sur la question d\'origine créée dans l\'outil d\'apprentissage externe.',
+	'mainBodyDescription': 'La mise à jour des détails de ces questions sera appliquée au questionnaire dans Brightspace, mais n'aura pas d'incidence sur la question d'origine créée dans l'outil d'apprentissage externe.',
 	'mainBodyWarning': 'Remarque : Pour tout questionnaire de Brightspace comportant des activités externes, les élèves verront une question par page.',
 	'mainBodyTitle': 'Mettre à jour les points pour les questions'
 };
